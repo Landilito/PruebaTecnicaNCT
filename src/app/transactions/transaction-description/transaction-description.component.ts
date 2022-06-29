86a7eb532683d4e2a17e18d2a0b5951382ecf171
Landy <Landilito10@gmail.com>
2022-06-29 12:52:32 -0300

    Generating the component to manage descriptions for transactions and including it in the transactions module

---
+++ src/app/transactions/transaction-description/transaction-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-description',
+  templateUrl: './transaction-description.component.html',
+  styleUrls: ['./transaction-description.component.css']
+})
+export class TransactionDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/transactions/transactions.module.ts
+++ src/app/transactions/transactions.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { TransactionRoutingModule } from './transaction-routing.module';
+import { TransactionDescriptionComponent } from './transaction-description/transaction-description.component';
 
 
 
 @NgModule({
   declarations: [
-    TransactionListComponent
+    TransactionListComponent,
+    TransactionDescriptionComponent
+
   ],
   imports: [
     CommonModule,
     TransactionRoutingModule
   ],
   exports: [
-    TransactionListComponent
+    TransactionListComponent,
+    TransactionDescriptionComponent
   ]
 })
 export class TransactionsModule { }
