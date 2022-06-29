aa9cbed4cf6040149778526f80328075474fa83a
Landy <Landilito10@gmail.com>
2022-06-29 18:50:56 -0300

    Setting a default reoute for the app, and chaging the template rendered by app.component.html to the router's

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
-import { transition } from '@angular/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{path: 'transactions', loadChildren: ()=>import('./transactions/transactions.module').then(m => m.TransactionsModule) }];
+const routes: Routes = [{path:'', redirectTo: 'transactions', pathMatch: 'full'},
+                        {path: 'transactions', loadChildren: ()=>import('./transactions/transactions.module').then(m => m.TransactionsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
