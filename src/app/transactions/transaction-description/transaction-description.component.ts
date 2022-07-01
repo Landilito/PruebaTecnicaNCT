03972857866597bbc92147ff9f06c9e3d80dff58
Landy <Landilito10@gmail.com>
2022-06-30 23:24:13 -0300

    Creating and adding to form, the form controls for the transaction-description. Creating selectById for the transactions list

--- src/app/transactions/transaction-description/transaction-description.component.ts
+++ src/app/transactions/transaction-description/transaction-description.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { TransactionService } from 'src/app/services/transaction.service';
+import { Transaction } from '../transaction';
 
 @Component({
   selector: 'app-transaction-description',
@@ -8,9 +11,40 @@ import { TransactionService } from 'src/app/services/transaction.service';
 })
 export class TransactionDescriptionComponent implements OnInit {
 
-  constructor(private transactionService: TransactionService) { }
+  transaction!: Transaction;
+  transactionForm!: FormGroup;
+
+  constructor(private transactionService: TransactionService, private router: Router, 
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.transactionForm = this.setupForm();
+    this.searchTransactionById();
+    const dto = this.transaction;
+    this.transactionForm.patchValue({...dto})
+     
+  }
+
+  setupForm(): FormGroup{
+    return this.fb.group({
+      concept: [null, Validators.required],
+      ammount: [null, Validators.required],
+      date: [null, Validators.required], 
+      description: [null, Validators.required]
+    })
+  }
+
+
+  searchTransactionById(){
+    const transactionId = this.router.url.split('/')[2];
+    const transactions = this.transactionService.getTransactions().filter(transaction => transaction.id === transactionId);
+    this.transaction = transactions[0];
+    console.log(this.transaction);
+  }
+
+  updateTransaction(transaction: Transaction){
+    this.transactionService.updateTransaction(transaction).subscribe(transaction=>{},
+    (err)=> console.error(err))
   }
 
 }
