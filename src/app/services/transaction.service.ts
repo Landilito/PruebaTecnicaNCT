3288dd97a1251289dbcc1c714b65c2b8f45bb793
Landy <Landilito10@gmail.com>
2022-06-30 23:20:12 -0300

    Creating updateTransaction method for edit section. Creating get and set for transactions retrieved by endpoint

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,20 +1,32 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Transaction } from '../transactions/transaction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
+  transactions: Transaction[] = [];
+
+  setTransactions(transactions:Transaction[]){
+    this.transactions = transactions;
+  }
+
+  getTransactions(): Transaction[] {
+    return this.transactions;
+  }
+
   constructor(private httpClient: HttpClient) { }
 
-  getTransactions(): Observable<any>{
+  getAllTransactions(): Observable<any>{
     return this.httpClient.get(`${environment.api_url}/transactions`)
   }
 
-  getTransactionsById(): Observable<any>{
-    return this.httpClient.get(`${environment.api_url}/transactions/id`)
+  updateTransaction(transaction: Transaction): Observable<any>{
+    return this.httpClient.put(`${environment.api_url}/transactions`, transaction)
   }
+
 }
