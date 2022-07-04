57b91d71d8c131fa9269aad774cdee06966051ac
Landy <Landilito10@gmail.com>
2022-07-04 11:21:33 -0300

    Setting a new route to access the create-transaction component and its template

--- src/app/transactions/transaction-routing.module.ts
+++ src/app/transactions/transaction-routing.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { TransactionDescriptionComponent } from './transaction-description/transaction-description.component';
+import { CreateTransactionsComponent } from './create-transactions/create-transactions.component';
 
 const routes: Routes = [{path:'', component: TransactionListComponent},
-                        {path:'transaction-description/:id', component: TransactionDescriptionComponent}]
+                        {path:'transaction-description/:id', component: TransactionDescriptionComponent},
+                        {path: 'create-transaction', component: CreateTransactionsComponent}]
 
 @NgModule({
   declarations: [],
