afa0b82bd925890dfef629b9e5dd73fe1aa73547
Landy <Landilito10@gmail.com>
2022-07-04 11:20:30 -0300

    Adding the create-transactions component to the transactions module

--- src/app/transactions/transactions.module.ts
+++ src/app/transactions/transactions.module.ts
@@ -3,22 +3,33 @@ import { CommonModule } from '@angular/common';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionDescriptionComponent } from './transaction-description/transaction-description.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CreateTransactionsComponent } from './create-transactions/create-transactions.component';
+import { NgxMaskModule } from 'ngx-mask';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
   declarations: [
     TransactionListComponent,
-    TransactionDescriptionComponent
+    TransactionDescriptionComponent,
+    CreateTransactionsComponent
 
   ],
   imports: [
     CommonModule,
-    TransactionRoutingModule
+    TransactionRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxMaskModule.forRoot(),
+
   ],
   exports: [
     TransactionListComponent,
-    TransactionDescriptionComponent
+    TransactionDescriptionComponent,
+    CreateTransactionsComponent
   ]
 })
 export class TransactionsModule { }
